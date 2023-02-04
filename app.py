6e36737e3d6f5501c3bb49471657c87004d74d22
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2023-02-04 15:12:47 +0700

    ready for demo

--- app.py
+++ app.py
@@ -12,14 +12,6 @@ def data_analysis():
     print(request)
     image = request.form['image'].replace('data:image/png;base64,','')
     return detect.Detect(image)
-
-# Detect v2
-@app.route('/detectData1', methods = ['POST'])
-def data_analysis1():
-    print(request)
-    image = request.form['image'].replace('data:image/png;base64,','')
-    return detect1.Detect(image)
-
     
 if __name__ == '__main__':
     app.run(debug = True)
